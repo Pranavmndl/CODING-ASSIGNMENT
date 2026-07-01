1a88e25393824571abd567e9a9c5aec0b5b8a1b8
Pranav Kumar <pranavmndl9@gmail.com>
2026-07-01 22:10:40 +0530

    Revese an array

---
+++ ReverseArray.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+    int n;
+    // Input array size
+    scanf("%d", &n);
+    int arr[n];
+    // Input array elements
+    for (int i = 0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    // Reverse the array
+    for (int i = 0; i<n/2; i++){
+        int temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+    // Print the reversed array
+    for (int i = 0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
