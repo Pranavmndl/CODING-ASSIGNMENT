7e4fe3fa3eb9e4e718394bbda3478f636b0e9b8d
Pranav Kumar <pranavmndl9@gmail.com>
2026-07-15 23:02:05 +0530

    two elements whose sum is nearest to 0

---
+++ SumNearestTo0.c
@@ -0,0 +1,56 @@
+// Problem: Given an array of integers, find two elements whose sum is closest to zero.
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers
+
+// Output:
+// - Print the pair of elements whose sum is closest to zero
+
+// Example:
+// Input:
+// 5
+// 1 60 -10 70 -80
+
+// Output:
+// -10 1
+
+// Explanation: Among all possible pairs, the sum of -10 and 1 is -9, which is the closest to zero compared to other pairs.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    int first=arr[0];
+    int second=arr[1];
+//initial closest sum
+    int closestSum= abs(arr[0]+arr[1]);
+
+    //checking every possible pair
+    for(int i=0; i<n-1; i++){
+        for(int j=i+1; j<n; j++){
+            int currentSum=arr[i]+arr[j];
+            //comparing absolute values
+            if(abs(currentSum)<closestSum){
+                closestSum=abs(currentSum);
+                first=arr[i];
+                second=arr[j];
+            }
+        }
+    }
+    //Print smaller element first
+    if(first<second){
+        printf("%d %d",first,second);
+    }else{
+        printf("%d %d",second,first);
+    }
+    return 0;
+
+}
